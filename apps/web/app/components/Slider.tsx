80aecf31960b1574e9b966d64c581d1178fa8cb2
keal khondekar <kewalkhondekar25@gmail.com>
2024-05-19 18:10:57 +0530

    feat: slider implementated

--- apps/web/app/components/Slider.tsx
+++ apps/web/app/components/Slider.tsx
@@ -1,55 +1,60 @@
 "use client"
 import React from 'react'
-import { Button } from './ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { SliderToggleBtn } from './buttons/Buttons';
 import { useSession } from 'next-auth/react';
+import {Pencil2Icon} from "@radix-ui/react-icons"
+import {CalendarIcon} from "@radix-ui/react-icons"
+
 
 
 const Slider = () => {
   const { data: session, status } = useSession();
-  const fruits: string[] = [
-    "Apple",        // A
-    "Banana",       // B
-    "Cherry",       // C
-    "Date",         // D
-    "Elderberry",   // E
-    "Fig",          // F
-    "Grape",        // G
-    "Honeydew",     // H
-    "Indian Fig",   // I
-    "Jackfruit",    // J
-    "Kiwi",         // K
-    "Lemon",        // L
-    "Mango",        // M
-    "Nectarine",    // N
-    "Orange",       // O
-    "Papaya",       // P
-    "Quince",       // Q
-    "Raspberry",    // R
-    "Strawberry",   // S
-    "Tangerine",    // T
-    "Ugli Fruit",   // U
-    "Voavanga",     // V
-    "Watermelon",   // W
-    "Xigua",        // X
-    "Yellow Passion Fruit", // Y
-    "Zucchini"      // Z
-  ];
-  
+  const subCategory: string[] = ["today", "upcoming"]
+  const iconMap: any = {
+    today: Pencil2Icon,
+    upcoming: CalendarIcon,
+  };
+
   return (
-    <section className='absolute top-0 bg-[#191919] flex justify-between h-full w-60'>
-      <nav>
-        {
-          fruits.map(item => {
-            return(
-              <ul>
-                <li>{item}</li>
-              </ul>
-            )
-          })
-        }
-      </nav>
-      <SliderToggleBtn/>
+    <section className='absolute top-0 bg-[#191919] h-full w-60'>
+      <div className='flex justify-between'>
+        <div>
+          <div className='flex justify-center place-items-center gap-2'>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p>
+              {session? JSON.stringify(session?.user?.name).replace(/"/g, '') : null}
+            </p>
+          </div>
+          <nav className='flex flex-col mt-10 ms-3'>
+            <div className='flex place-items-center gap-2'>
+              <Pencil2Icon/>
+              <p>Todos</p>
+            </div>
+            <div className='mt-2 ms-5'>
+              {
+                subCategory.map((item, i) => {
+                  const IconComponent = iconMap[item];
+                  const today = new Date();
+                  const date = today.getDate();  
+                  return(
+                    <div key={i} className='flex place-items-center gap-1'>
+                      {item === "today" ? date : IconComponent && <IconComponent className='' />}
+                      <ul>
+                        <li className='text-sm capitalize'>{item}</li>
+                      </ul>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </nav>
+        </div>
+        <SliderToggleBtn/>
+      </div>
     </section>
   )
 }
