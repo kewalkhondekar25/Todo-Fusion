405050b80972bc62777aabf64ac7fd8dae9089d3
keal khondekar <kewalkhondekar25@gmail.com>
2024-05-28 20:29:22 +0530

    feat: group todos added

--- apps/web/app/components/Todos/UpcomingTodos/UpcomingTodos.tsx
+++ apps/web/app/components/Todos/UpcomingTodos/UpcomingTodos.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect } from 'react'
-import { useAppSelector } from '../../../../lib/store/hooks/hooks'
+import { useAppDispatch, useAppSelector } from '../../../../lib/store/hooks/hooks'
+import { setUpcomingTodos } from '../../../../lib/store/features/todos/todoSlice';
 import axios from 'axios';
 
 type AllTodosType = {
@@ -10,37 +11,59 @@ type AllTodosType = {
   createdAt: Date;
   updatedAt: Date;
   usersId: string;
-  priority: string;
-  date: Date;
-  hours: string;
-  minutes: string;
-  isUpcoming: boolean;
+  priority?: string;
+  date?: Date;
+  hours?: string;
+  minutes?: string;
+  isUpcoming?: boolean;
 }
 
 const UpcomingTodos = () => {
+
+  const dispatch = useAppDispatch();
+  const { upcomingTodos } = useAppSelector(state => state.todo)
+
   const getUpcomingTodos = async () => {
     const response = await axios.get("/api/todaystodos");
     const allTodos = response.data as AllTodosType[];
     const futureTodos = allTodos.filter(item => item.isUpcoming === true);
-    console.log(futureTodos);
-    
-    
-  } 
+    dispatch(setUpcomingTodos(futureTodos));
+  }
 
   useEffect(() => {
     getUpcomingTodos();
-  }, [])
+  }, []);
+
+  const groupTodosByDate = (todos: AllTodosType[]) => {
+    return todos.reduce((acc, todo) => {
+      if (todo.date) {
+        const formattedDate = new Date(todo.date).toLocaleDateString(undefined, { month: 'long', day: 'numeric' });
+        if (!acc[formattedDate]) {
+          acc[formattedDate] = [];
+        }
+        acc[formattedDate].push(todo);
+      }
+      return acc;
+    }, {} as { [key: string]: AllTodosType[] | any });
+  }
+
+  const groupedTodos = groupTodosByDate(upcomingTodos);
+
   return (
     <div>
-      {/* {
-        upcomingTodos.map(item => {
-          return(
-            <ul>
-              <li>{item.todo}</li>
-            </ul>
-          )
-        })
-      } */}
+      {
+        Object.entries(groupedTodos).map(([date, todos]) => (
+          <div key={date}>
+            <h3>{date}</h3>
+            {todos && todos.map((todo: AllTodosType) => ( // Specify the type of todo here
+              <div key={todo.id}>
+                <p>{todo.todo}</p>
+              </div>
+            ))}
+            <hr />
+          </div>
+        ))
+      }
     </div>
   )
 }
