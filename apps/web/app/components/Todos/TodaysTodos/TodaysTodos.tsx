859f46c6e40f4a8bd5fa7dc7a8d12498787900f5
keal khondekar <kewalkhondekar25@gmail.com>
2024-05-29 16:46:52 +0530

    fix: removed old todos

--- apps/web/app/components/Todos/TodaysTodos/TodaysTodos.tsx
+++ apps/web/app/components/Todos/TodaysTodos/TodaysTodos.tsx
@@ -4,7 +4,7 @@ import { getAllTodos } from '@repo/db'
 import { TodaysTodoCheckBox } from '../../checkboxes/CheckBoxes';
 import axios from 'axios';
 import { useAppDispatch, useAppSelector } from '../../../../lib/store/hooks/hooks';
-import {ToggleEditTodo, setAddedTodoStatus, setTodos, setEditTodo, setEditValues, setUpcomingTodos} from "../../../../lib/store/features/todos/todoSlice"
+import { ToggleEditTodo, setAddedTodoStatus, setTodos, setEditTodo, setEditValues, setUpcomingTodos } from "../../../../lib/store/features/todos/todoSlice"
 import { Checkbox } from "../../ui/checkbox"
 import { toast } from 'sonner';
 import { Pencil1Icon, TrashIcon } from '@radix-ui/react-icons';
@@ -29,21 +29,24 @@ const TodaysTodos = () => {
     id: "",
     todo: ""
   });
-  console.log(editPayload);
-  
-  const {todos, todoCount, isEditTodoOpen, editValues} = useAppSelector(state => state.todo);
-  const dispatch = useAppDispatch();  
+  const { todos, todoCount, isEditTodoOpen, editValues } = useAppSelector(state => state.todo);
+  const dispatch = useAppDispatch();
 
   const getTodaysTodo = async () => {
     try {
       const response = axios.get("/api/todaystodos");
       const data = (await response).data as AllTodosType[];
       const upcomingTodos = data.filter(item => item.isUpcoming === true);
+      //filter today todos
       const todaysTodos = data.filter(item => item.isUpcoming === false);
-      console.log("today'''s todos are: ", todaysTodos);
+      //remove yesterday todos
+      const today = new Date().toISOString().split('T')[0];
+      const presentTodos = todaysTodos.filter(item => {
+          const todoDate = new Date(item.createdAt).toISOString().split('T')[0];
+          return todoDate === today;
+      });
       dispatch(setUpcomingTodos(upcomingTodos));
-      dispatch(setTodos(todaysTodos))
-      console.log(data);
+      dispatch(setTodos(presentTodos))
     } catch (error) {
       console.log(error);
     }
@@ -52,57 +55,55 @@ const TodaysTodos = () => {
   const handleCompleteTodo = async (todoId: string) => {
     try {
       //post id as payload
-      const response = await axios.post("/api/iscomplete", {id: todoId});
+      const response = await axios.post("/api/iscomplete", { id: todoId });
       const data = response.data;
       dispatch(setAddedTodoStatus(`${todoId}`));
       toast("You're on a roll! Another todo checked off your list.🎯", {
         description: ""
       })
-      console.log(data);
     } catch (error: any) {
       console.log(error.message);
     }
   };
 
   const handleEditPayload = (id: string, todo: string, priority: string, hours: string, minutes: string) => {
-    dispatch(setEditTodo({id, todo, priority, hours, minutes}));
-    dispatch(setEditValues({...editValues, id}))
+    dispatch(setEditTodo({ id, todo, priority, hours, minutes }));
+    dispatch(setEditValues({ ...editValues, id }))
   }
 
   useEffect(() => {
-    console.log(todoCount)
     getTodaysTodo()
   }, [todoCount])
 
-  return(
+  return (
     <div>
       {
         todos.map((item, i) => {
-          return(
+          return (
             <div key={i} className='flex place-items-center gap-2'>
-              <Checkbox 
+              <Checkbox
                 onCheckedChange={() => handleCompleteTodo(item.id)}
                 checked={item.isCompleted}
                 disabled={item.isCompleted}
               />
               <div className='relative flex-1 flex items-center group'>
-                
+
                 <div className={`hover:cursor-pointer ${item.isCompleted ? 'line-through ' : ''}flex-1`}>
                   {item.todo}
                 </div>
                 {item.isCompleted ? null : <div className='cursor-pointer ml-2 hidden group-hover:flex group-hover:place-items-center'>
-                  <Pencil1Icon 
+                  <Pencil1Icon
                     className='relative h-5 w-5'
-                    onClick={() => {handleEditPayload(item.id, item.todo, item.priority ?? "", item.hours ?? "", item.minutes ?? ""); dispatch(ToggleEditTodo())}}
+                    onClick={() => { handleEditPayload(item.id, item.todo, item.priority ?? "", item.hours ?? "", item.minutes ?? ""); dispatch(ToggleEditTodo()) }}
                   />
-                  <TrashIcon className='ms-2 h-5 w-5'/>
+                  <TrashIcon className='ms-2 h-5 w-5' />
                 </div>}
               </div>
             </div>
           )
         })
       }
-      {isEditTodoOpen && <EditTodo/>}
+      {isEditTodoOpen && <EditTodo />}
     </div>
   )
 }
